cecbc2cfda5662ca752ebf37ece6d672a0c1112e
Muiz <m.muiz.q@gmail.com>
2019-11-22 17:52:04 +1100

    added sign in with email

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shoppage/shop.components'
 import Signing from './pages/signing/signing.components'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 class App extends React.Component {
   constructor(props){
@@ -18,8 +18,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state)
+          })
+        })
+      }
+      this.setState({ currentUser: userAuth })
     })
   }
 
--- src/pages/signing/signing.components.jsx
+++ src/pages/signing/signing.components.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.components'
+import SignUp from '../../components/sign-up/sign-up.component'
+
+import './signing.styles.scss'
 
 const Signing = () => (
-    <SignIn />
+    <div className='signing'>
+        <SignIn />
+        <SignUp />
+    </div>
 )
 
 export default Signing;
