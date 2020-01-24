bc8cc74c2ff347921e2a6e09f4cabb9f5add08da
Muiz <m.muiz.q@gmail.com>
2020-01-24 12:40:54 +1100

    Added redux-sagas to preview collections

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,15 +4,19 @@ import rootReducer from './root-reducer';
 import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga'
+import { fetchCollectionsStart } from './shop/shop.sagas';
+import rootSaga from './root-saga.js'
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if(process.env.NODE_ENV === 'development'){
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
+
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store)
 
