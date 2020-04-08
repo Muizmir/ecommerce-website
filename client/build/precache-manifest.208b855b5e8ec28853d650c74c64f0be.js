ea14403501baca833f90c01cee0adfe7533d4f6c
Muiz <m.muiz.q@gmail.com>
2020-04-08 21:35:23 +1000

    fixed issues

---
+++ client/build/precache-manifest.208b855b5e8ec28853d650c74c64f0be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbedd19a8cd327c8b07f2737291dfbe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "400281c7b2cbb3835930",
+    "url": "/static/css/main.23b320c5.chunk.css"
+  },
+  {
+    "revision": "1c9d3c386f485d4f2c6e",
+    "url": "/static/js/2.cb744944.chunk.js"
+  },
+  {
+    "revision": "400281c7b2cbb3835930",
+    "url": "/static/js/main.46ed87fc.chunk.js"
+  },
+  {
+    "revision": "eaa6dc51e76a7d1c5e8b",
+    "url": "/static/js/runtime-main.f8dfab26.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
